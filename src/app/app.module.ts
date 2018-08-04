e2126782c2975e20766a561e2dc900a89fac8316
mikedobor <micki1.com@gmail.com>
2018-08-05 00:04:34 +0100

    Project initialization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WelcomepageComponent } from './components/welcomepage/welcomepage.component';
+import { UserpageComponent } from './components/userpage/userpage.component';
+import { MessagingComponent } from './components/messaging/messaging.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomepageComponent,
+    UserpageComponent,
+    MessagingComponent
   ],
   imports: [
     BrowserModule
